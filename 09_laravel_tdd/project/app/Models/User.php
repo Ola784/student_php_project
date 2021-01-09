d293cf41eb5fad254445f80d8c10c78d93ad6b88
Maja Pegiel <maja@pegiel.pl>
2021-01-09 11:03:38 +0100

    Create page migration,table,controller, add relationship many to many

--- 09_laravel_tdd/project/app/Models/User.php
+++ 09_laravel_tdd/project/app/Models/User.php
@@ -41,4 +41,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function pages(){
+        return $this->belongsToMany(Page::class);
+    }
 }
