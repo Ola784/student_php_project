8ce370f3b0741c029210f7bf963902b5d1b7684b
Maja Pegiel <maja@pegiel.pl>
2021-01-13 13:22:53 +0100

    change navigation

--- 09_laravel_tdd/project/resources/views/layouts/navigation.blade.php
+++ 09_laravel_tdd/project/resources/views/layouts/navigation.blade.php
@@ -15,9 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('menus.index')" :active="request()->routeIs('books.*')">
-                        {{ __('Menu') }}
-                    </x-nav-link>
+
                 </div>
             </div>
 
