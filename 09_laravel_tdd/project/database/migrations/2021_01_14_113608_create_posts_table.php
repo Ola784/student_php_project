ad4dbd4bda6067862225c4a87b32bd5cd9c781ad
Daria Gryla <dariagryla@student.agh.edu.pl>
2021-01-21 19:13:10 +0100

    Edit in categories and posts + adding tags

--- 09_laravel_tdd/project/database/migrations/2021_01_14_113608_create_posts_table.php
+++ 09_laravel_tdd/project/database/migrations/2021_01_14_113608_create_posts_table.php
@@ -19,9 +19,6 @@ class CreatePostsTable extends Migration
             $table->string('slug')->unique();
             $table->string('image')->default('default.png');
             $table->text('body');
-            $table->integer('view_count')->default(0);
-            $table->boolean('status')->default(false);
-            $table->boolean('is_approved')->default(false);
             $table->timestamps();
 
             //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
